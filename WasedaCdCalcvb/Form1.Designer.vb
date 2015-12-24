d39f36b3852329f8faefc64113f7295e80e150d6
tubone24 <meitantei-conan@live.jp>
2015-12-24 17:33:29 +0900

    First Commit.

---
+++ WasedaCdCalcvb/Form1.Designer.vb
@@ -0,0 +1,82 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxSId = New System.Windows.Forms.TextBox()
+        Me.Calc = New System.Windows.Forms.Button()
+        Me.TextBoxAnswer = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBoxSId
+        '
+        Me.TextBoxSId.Location = New System.Drawing.Point(64, 44)
+        Me.TextBoxSId.Name = "TextBoxSId"
+        Me.TextBoxSId.Size = New System.Drawing.Size(100, 19)
+        Me.TextBoxSId.TabIndex = 0
+        '
+        'Calc
+        '
+        Me.Calc.Location = New System.Drawing.Point(64, 97)
+        Me.Calc.Name = "Calc"
+        Me.Calc.Size = New System.Drawing.Size(75, 23)
+        Me.Calc.TabIndex = 1
+        Me.Calc.Text = "Calc!"
+        Me.Calc.UseVisualStyleBackColor = True
+        '
+        'TextBoxAnswer
+        '
+        Me.TextBoxAnswer.Location = New System.Drawing.Point(64, 159)
+        Me.TextBoxAnswer.Name = "TextBoxAnswer"
+        Me.TextBoxAnswer.Size = New System.Drawing.Size(100, 19)
+        Me.TextBoxAnswer.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(64, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(162, 19)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "入力例: 1H15A010"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBoxAnswer)
+        Me.Controls.Add(Me.Calc)
+        Me.Controls.Add(Me.TextBoxSId)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBoxSId As System.Windows.Forms.TextBox
+    Friend WithEvents Calc As System.Windows.Forms.Button
+    Friend WithEvents TextBoxAnswer As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ WasedaCdCalcvb/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub Calc_Click(sender As Object, e As System.EventArgs) Handles Calc.Click
+        Dim input_check_digit = TextBoxSId.Text
+
+        If System.Text.RegularExpressions.Regex.IsMatch( _
+        TextBoxSId.Text, "^[A-I0-9]{8}$") Then '学籍番号形式確認
+            TextBoxAnswer.Text = calcCd(replaceSn(TextBoxSId.Text))
+        Else
+            MsgBox("ちゃんと学籍番号入力して！")
+
+        End If
+    End Sub
+
+    Function replaceSn(inputN) 'アルファベットを数字に変換
+        Dim after_cd
+        Dim input_check_digit = inputN
+        after_cd = input_check_digit.replace("A", "1")
+        after_cd = after_cd.replace("B", "2")
+        after_cd = after_cd.replace("C", "3")
+        after_cd = after_cd.replace("D", "4")
+        after_cd = after_cd.replace("E", "5")
+        after_cd = after_cd.replace("F", "6")
+        after_cd = after_cd.replace("G", "7")
+        after_cd = after_cd.replace("H", "8")
+        after_cd = after_cd.replace("I", "9")
+        Return after_cd
+    End Function
+
+    Function calcCd(ByVal input_check_digit)
+        Dim split_cdc() As Char = input_check_digit.ToCharArray '入力をChar型の配列に落とす
+        Dim split_cdi(8) As Integer
+        For i = 0 To 7
+            split_cdi(i) = Integer.Parse(split_cdc(i)) '計算するために数値変換
+        Next
+        Dim answer = 11 - (split_cdi(1) * 2 + split_cdi(3) * 3 + split_cdi(4) * 4 + split_cdi(5) * 5 + split_cdi(6) * 6 + split_cdi(7) * 7) Mod 11
+        Return answer
+    End Function
+
+
+End Class
